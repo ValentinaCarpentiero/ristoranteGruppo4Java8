ba696a24a71d84ea4deef3c229c84c1da343914b
Valentina Carpentiero <carpentierovalentina@gmail.com>
2022-11-15 19:07:36 +0100

    creata classe menu e classe tester

---
+++ src/src/Menu.java
@@ -0,0 +1,20 @@
+
+public class Menu {
+    private String nameMenu;
+    private String menuType;
+    private String restaurantName;
+
+
+
+
+    public Menu(String nameMenu, String menuType, String restaurantName) {
+        this.nameMenu = nameMenu;
+        this.menuType = menuType;
+        this.restaurantName = restaurantName;
+
+
+    }
+
+
+
+}
