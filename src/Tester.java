e2a533fae82c23ceb0e77be5d50fdf71bad1182d
Valentina Carpentiero <carpentierovalentina@gmail.com>
2022-11-24 13:06:14 +0100

    TODO done

--- src/Tester.java
+++ src/Tester.java
@@ -1,16 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class Tester {
     public static void main(String[] args) {
 
-        ArrayList<Dish> dishList = new ArrayList<>();
-
         Dish firstCourse1 =new FirstCourse(12.55, "Spaghetti allo scoglio","First Course", "Spaghetti con vongole, cozze, calamari");
         Dish secondCourse1 = new SecondCourse(17.00, "Orata al forno con patate", "Second Course","Orata, patate, rosmarino, aglio");
         Dish dessert1 = new Dessert(6.50, "Torta al cioccolato", "Dessert", "Cioccolato fondente, farina 00, uova");
-        Dish drink1 = new Drink(6.00,"Lapilli","Vino","Bianco");
+        Dish drink1 = new Drink(6.00,"Lapilli","Wine","Bianco");
         Menu menu = new Menu("Pesce", "Restaurant Java 8 [Gruppo 4]");
+
 
         menu.addDish(firstCourse1);
         menu.addDish(secondCourse1);
